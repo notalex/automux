12992a1b195f1a6240d083903159d720bfd59b2b
Alex Johnson <notalexjohnson@gmail.com>
2013-03-17 16:15:44 +0530

    exec all commands together

--- automux.rb
+++ automux.rb
@@ -3,16 +3,20 @@ require 'yaml'
 data = YAML.load_file('/home/alex/delete/sample.yml')
 
 class Tmux
-  attr_reader :data, :session_name
+  attr_reader :data, :session_name, :commands
 
   def initialize(data)
     @data = data
     @session_name = data['project_name']
   end
 
+  def commands
+    @commands ||= []
+  end
+
   def do
-    system %[tmux start-server]
-    system %[tmux -u2 new-session -d -s #{ session_name }]
+    commands << "\n" + %[tmux start-server]
+    commands << "\n" + %[tmux -u2 new-session -d -s #{ session_name }]
     data['tabs'].each_with_index do |tab, i|
       new_window(i, tab.keys.first) if i > 0
       options = tab.values.first
@@ -27,32 +31,33 @@ class Tmux
       end
     end
     attach_session
+    exec(commands.join)
   end
 
 private
 
   def new_window(number, name)
-    system %[tmux new-window -t #{ session_name }:#{ number } -n #{ name }]
+    commands << "\n" + %[tmux new-window -t #{ session_name }:#{ number } -n #{ name }]
   end
 
   def run_command(number, command)
-    system %[tmux send-keys -t #{ session_name }:#{ number } "#{ command }" C-m]
+    commands << "\n" + %[tmux send-keys -t #{ session_name }:#{ number } "#{ command }" C-m]
   end
 
   def attach_session
-    system %[tmux -u2 attach-session -t #{ session_name }]
+    commands << "\n" + %[tmux -u2 attach-session -t #{ session_name }]
   end
 
   def select_layout(number, name)
-    system %[tmux select-layout -t #{ session_name }:#{ number } #{ name }]
+    commands << "\n" + %[tmux select-layout -t #{ session_name }:#{ number } #{ name }]
   end
 
   def create_pane
-    system %[tmux split-window]
+    commands << "\n" + %[tmux split-window]
   end
 
   def send_command(command)
-    system %[tmux send-keys #{ command } C-m]
+    commands << "\n" + %[tmux send-keys #{ command } C-m]
   end
 end
 
