c8faaab3fe49a96cf47591245dd24be5afaa82d7
Alex Johnson <notalexjohnson@gmail.com>
2013-03-25 19:48:30 +0530

    fix: dup_attr_reader to define multiple readers

--- lib/automux/core/support/custom_accessors.rb
+++ lib/automux/core/support/custom_accessors.rb
@@ -2,9 +2,11 @@ module Automux
   module Core
     module Support
       module CustomAccessors
-        def dup_attr_reader(name)
-          define_method name do
-            instance_variable_get("@#{ name }").dup
+        def dup_attr_reader(*names)
+          names.each do |name|
+            define_method name do
+              instance_variable_get("@#{ name }").dup
+            end
           end
         end
       end
