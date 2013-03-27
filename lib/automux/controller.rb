ce40bdab2a05f3526f4df758fb23ece6e4ad42ba
Alex Johnson <notalexjohnson@gmail.com>
2013-03-27 16:36:57 +0530

    fix: remove require for non existing file

--- lib/automux/controller.rb
+++ lib/automux/controller.rb
@@ -1,7 +1,6 @@
 require 'automux/controller/support'
 require 'automux/controller/base'
 require 'automux/controller/recipes'
-require 'automux/controller/blueprints'
 
 module Automux::Controller
   class << self
