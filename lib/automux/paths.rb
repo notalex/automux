51a425accf6858c76ec5db303b8e721e66fe1d16
Alex Johnson <notalexjohnson@gmail.com>
2013-03-27 19:27:22 +0530

    refactoring to remove redundant method

--- lib/automux/paths.rb
+++ lib/automux/paths.rb
@@ -24,12 +24,8 @@ module Automux
 
     private ###
 
-    def user_data(type)
-      File.join(user_assets, type, '*')
-    end
-
     def paths_for(type)
-      Dir[user_data(type), File.join(data, type, '*')]
+      Dir[File.join(user_assets, type, '*'), File.join(data, type, '*')]
     end
   end
 end
