5b8a2ce5202902f0397d7c9127bb21bdc26112b5
Alex Johnson <notalexjohnson@gmail.com>
2013-03-29 01:02:35 +0530

    step definition for asserting inclusion of command

--- features/step_definitions/shared_steps.rb
+++ features/step_definitions/shared_steps.rb
@@ -18,12 +18,34 @@ When(/^Automux processes this blueprint with the following options$/) do |table|
 end
 
 Then(/^the rendered sequence of shell commands should be$/) do |string|
-
-  results_array = @results.split("\n")
-  expected_string = string.gsub(/\$HOME/, ENV['HOME'])
-  expected_array = expected_string.split("\n").reject { |text| text.split.empty? }
+  expected_array, results_array = expected_and_results_array(string)
 
   expected_array.length.times do |i|
-    assert_equal(expected_array[i].strip, results_array[i].strip)
+    assert_equal(expected_array[i], results_array[i])
   end
+end
+
+Then(/^the rendered sequence of shell commands should contain$/) do |string|
+  expected_array, results_array = expected_and_results_array(string)
+
+  expected_array.each do |command|
+    assert_includes(results_array, command)
+  end
+end
+
+def expected_and_results_array(string)
+  results_array = @results.split("\n").map { |text| text.strip }
+  expected_array = expected_array_for(string)
+  [expected_array, results_array]
+end
+
+def expected_array_for(string)
+  expected_string = string.gsub(/\$HOME/, ENV['HOME'])
+  expected_array = expected_string.split("\n").reject { |text| text.split.empty? }
+  expected_array.map { |text| text.strip }
+end
+
+When(/^I invoke Automux with the blueprint "(.*?)"$/) do |name|
+  system %[bin/automux #{ name }]
+  @results = File.read('/tmp/results')
 end
