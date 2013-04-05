a61752bf20d2ced99b8a37396a603f0fe15e8771
Alex Johnson <notalexjohnson@gmail.com>
2013-04-06 01:51:44 +0530

    removing currently unused method

--- lib/automux/core/base.rb
+++ lib/automux/core/base.rb
@@ -2,12 +2,6 @@ module Automux
   module Core
     class Base
       extend Automux::Core::Support::CustomAccessors
-
-      private ###
-
-      def saved_record?
-        FileTest.exists?(path)
-      end
     end
   end
 end
