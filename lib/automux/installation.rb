f3cb26e5747fdb2a4c537fa62112769ff69b6b3c
Alex Johnson <notalexjohnson@gmail.com>
2013-04-06 01:59:13 +0530

    correcting installation message indentation

--- lib/automux/installation.rb
+++ lib/automux/installation.rb
@@ -1,10 +1,10 @@
 module Automux
   module Installation
-    MESSAGE = <<-END
-********************************************************************************
-Please run the following the following command to setup default Automux folders:
-$ automux setup
-********************************************************************************
+    MESSAGE = <<-END.gsub(/^\s+/, '')
+      ********************************************************************************
+      Please run the following the following command to setup default Automux folders:
+      $ automux setup
+      ********************************************************************************
     END
   end
 end
